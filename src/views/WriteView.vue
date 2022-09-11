52e7938c71ce0d3ad3a3c86ce8ac3e7a9e6f8b89
hclee <hclee@dataeum.kr>
2022-09-11 18:29:01 +0900

    글등록 추가

---
+++ src/views/WriteView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+import axios from 'axios';
+
+const title = ref("");
+const content = ref("");
+
+const write = function () {
+  axios.post("http://localhost:9000/api/posts", {
+    title: title.value,
+    content: content.value
+  })
+
+};
+
+</script>
+
+<template>
+
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력하세요."/>
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15" placeholder="내용을 입력하세요."/>
+  </div>
+
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">글 작성 완료</el-button>
+  </div>
+
+</template>
+
+<style>
+
+</style>
